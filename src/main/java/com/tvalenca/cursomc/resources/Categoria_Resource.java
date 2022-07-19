200dd26811fe863c0f85fdddb2f3f29bcab1c562
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-07-19 10:49:07 -0300

    Resources | Categoria

---
+++ src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
@@ -0,0 +1,15 @@
+package com.tvalenca.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class Categoria_Resource {
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public String Listar(){
+        return "Rest está funcionando";
+    }
+}
