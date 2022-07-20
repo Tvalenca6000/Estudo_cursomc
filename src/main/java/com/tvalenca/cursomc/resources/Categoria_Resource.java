c24d74adcfcffbdadca9bad17fff1edee4a06d78
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-07-20 11:40:05 -0300

    Resources | Testando Categoria

--- src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
+++ src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
@@ -1,15 +1,27 @@
 package com.tvalenca.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tvalenca.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class Categoria_Resource {
     
     @RequestMapping(method=RequestMethod.GET)
-    public String Listar(){
-        return "Rest está funcionando";
+    public List<Categoria> Listar(){
+        Categoria cat = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista =  new ArrayList<>();
+        lista.add(cat);
+        lista.add(cat2);
+
+        return lista;
     }
 }
