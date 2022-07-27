34c90c1f9ac06ec5208071a2cddb845b70a6d1ae
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-07-27 12:32:50 -0300

    Domain | Criação e teste Produto_Categoria

--- src/main/java/com/tvalenca/cursomc/CursomcApplication.java
+++ src/main/java/com/tvalenca/cursomc/CursomcApplication.java
@@ -1,13 +1,49 @@
 package com.tvalenca.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tvalenca.cursomc.domain.Categoria;
+import com.tvalenca.cursomc.domain.Produto;
+import com.tvalenca.cursomc.repositories.Categoria_Repository;
+import com.tvalenca.cursomc.repositories.Produto_Repository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private Categoria_Repository categoriaRepository;
+	@Autowired
+	private Produto_Repository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
