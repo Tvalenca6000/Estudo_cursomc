9ee787e3924dd521d53a01b80165936eac79b652
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-08-11 11:11:36 -0300

    Domain | Atualização utilizando @JsonIgnore

--- src/main/java/com/tvalenca/cursomc/domain/Categoria.java
+++ src/main/java/com/tvalenca/cursomc/domain/Categoria.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable {
@@ -21,7 +20,6 @@ public class Categoria implements Serializable {
     private Integer id;
     private String nome;
 
-    @JsonManagedReference
     @ManyToMany(mappedBy = "categorias")
     private List<Produto> produtos = new ArrayList<>();
 
--- src/main/java/com/tvalenca/cursomc/domain/Estado.java
+++ src/main/java/com/tvalenca/cursomc/domain/Estado.java
@@ -10,7 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Estado implements Serializable {
@@ -21,7 +22,7 @@ public class Estado implements Serializable {
     private Integer id;
     private String nome;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToMany(mappedBy = "estado")
     private List<Cidade> cidades = new ArrayList<>();
 
